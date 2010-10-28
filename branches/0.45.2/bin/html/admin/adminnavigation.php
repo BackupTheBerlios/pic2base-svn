868e573f981818e574ed99b4c856369845dccd4a
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2010-10-28 16:10:38 +0000

    Adminnavigation weiter rechteabhängig gemacht

--- branches/0.45.2/bin/html/admin/adminnavigation.php
+++ branches/0.45.2/bin/html/admin/adminnavigation.php
@@ -64,15 +64,28 @@ include '../../share/global_config.php';
     		break;
     	}
   
-    	echo "
-    	<a class='navi' href='$inst_path/pic2base/bin/ftp_frontend/?' title='FTP-Statistik'>FTP-Statistik</a>
-	<a class='navi' href='$inst_path/pic2base/log/p2b_log.php' title='pic2base-Logdatei'>p2b-Log</a>
-	<!--<a class='navi' href='$inst_path/pic2base/bin/admin/admin/md5_add.php' title='kontrolliert auf fehlende Pr&uuml;fsummen'>md5-Check</a>-->
-	<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_histogram0.php' title='erstellt fehlende Histogramme'>Histogramme</a>
-	<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_exifdata0.php' title='&uuml;bertr&auml;gt fehlende Meta-Daten aus den Bildern in die DB'>Meta-Daten</a>
-	<a class='navi' href='$inst_path/pic2base/bin/admin/admin/protect_metadata0.php' title='Festlegung editierbarer Meta-Daten'>Meta-Protect</a>
-	<a class='navi' href='$inst_path/pic2base/bin/admin/admin/kompactview_metadata0.php' title='Welche Meta-Daten werden in der kompakten Detailansicht gezeigt?'>Meta-View</a>
-	<a class='navi' href='$inst_path/pic2base/bin/admin/admin/check_software0.php' title='&uuml;berpr&uuml;ft, ob erforderliche Software installiert ist'>Software-Check</a>
+    	IF(hasPermission($c_username, 'adminlogin'))
+    	{
+	    	echo "
+	    	<a class='navi' href='$inst_path/pic2base/bin/ftp_frontend/?' title='FTP-Statistik'>FTP-Statistik</a>
+			<a class='navi' href='$inst_path/pic2base/log/p2b_log.php' title='pic2base-Logdatei'>p2b-Log</a>
+			<!--<a class='navi' href='$inst_path/pic2base/bin/admin/admin/md5_add.php' title='kontrolliert auf fehlende Pr&uuml;fsummen'>md5-Check</a>-->
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_histogram0.php' title='erstellt fehlende Histogramme'>Histogramme</a>
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_exifdata0.php' title='&uuml;bertr&auml;gt fehlende Meta-Daten aus den Bildern in die DB'>Meta-Daten</a>
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/protect_metadata0.php' title='Festlegung editierbarer Meta-Daten'>Meta-Protect</a>
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/kompactview_metadata0.php' title='Welche Meta-Daten werden in der kompakten Detailansicht gezeigt?'>Meta-View</a>
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/check_software0.php' title='&uuml;berpr&uuml;ft, ob erforderliche Software installiert ist'>Software-Check</a>";
+    	}
+    	ELSE
+    	{
+    		echo "	
+			<a class='navi_blind'></a>
+			<a class='navi_blind'></a>
+			<a class='navi_blind'></a>
+			<a class='navi_blind'></a>
+			<a class='navi_blind'></a>";
+    	}
+    echo "	
 	<a class='navi_blind'></a>
 	<a class='navi' href='$inst_path/pic2base/bin/html/start.php'>zur Startseite</a>
 	<a class='navi' href='$inst_path/pic2base/bin/html/help/help1.php?page=5'>Hilfe</a>
