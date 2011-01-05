11a778a713473343701a7845b6ae6769466e99c2
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2011-01-05 09:59:33 +0000

    Installations-Hilfsskript (bin/share/db_check1.php) wird beim erstmaligem Aufruf des Adminbereichs gelöscht

--- branches/0.50.0/html/admin/adminhome.php
+++ branches/0.50.0/html/admin/adminhome.php
@@ -1,2 +1,15 @@
 <p style="margin:120px 0px; text-align:center">Willkommen im Admin-Bereich!<BR><BR></BR>
 W&auml;hlen Sie bitte aus der linken Leiste die gew&uuml;nschte Aktion aus.</p>
+<?php
+include '../../share/global_config.php';
+$install_file = '../../share/db_check1.php';
+$fh = fopen($install_file, 'r');
+IF($fh)
+{
+	//echo "Datei konnte ge&ouml;ffnet werden.";
+}
+IF(fclose($fh) AND @unlink($install_file))
+{
+	echo "Installations-Hilfsskript wurde entfernt.";
+}
+?>
