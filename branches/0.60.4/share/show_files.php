8d26a076e07f25cec4836fa2f4c24e830ee0296b
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-07-29 09:33:44 +0000

    findet in der AJAX-basierten Stapelerfassung Verwendung

---
+++ branches/0.60.4/share/show_files.php
@@ -0,0 +1,40 @@
+<?php
+unset($username);
+IF ($_COOKIE['login'])
+{
+	list($c_username) = preg_split('#,#',$_COOKIE['login']);
+	//echo $c_username;
+	$benutzername = $c_username;
+}
+
+include 'global_config.php';
+
+//Ermittlung der Anzahl von Dateien im Uploadordner des angemeldeten Users:
+//echo "<BR>Upload-Ordner des Users: ".$up_dir."<BR><BR>";
+
+$n = 0;				//Zaehlvariable fuer die zu bearbeitenden Bilder (Bilder im Upload-Ordner)
+//Ermittlung, wieviel Bilddateien sich in dem angegebenen Ordner befinden und Abspeicherung der Dateinamen in einem Array:
+$bild_datei = array();
+$dh = opendir($up_dir);
+
+$obj = new stdClass();
+
+while($datei_name=readdir($dh))
+{
+	if($datei_name != "" && $datei_name != "." && $datei_name != "..")
+	{
+		$info = pathinfo($datei_name);
+		$extension = strtolower($info['extension']);
+		IF(in_array($extension,$supported_filetypes) OR $extension == 'jpg')
+		{
+			$bild_datei[] = $datei_name;
+			$n++;	
+		}
+	}
+}
+
+$obj->anzahl = $n;
+$obj->file_array = $bild_datei;
+$output = json_encode($obj);
+echo $output;
+?>
