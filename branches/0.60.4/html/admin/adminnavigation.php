5a11a734e1e97ce6059951ebab2d2549389046e9
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-08-07 16:36:18 +0000

    Navigationsstruktur angepasst (Histogramm-Erzeugung entfernt, DB-Export aufgenommen)

--- branches/0.60.4/html/admin/adminnavigation.php
+++ branches/0.60.4/html/admin/adminnavigation.php
@@ -90,9 +90,10 @@ include '../../share/global_config.php';
 		   	<a class='navi' href='$inst_path/pic2base/bin/ftp_frontend/?' title='FTP-Statistik'>FTP-Statistik</a>
 			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/p2b_log.php' title='pic2base-Logdatei'>p2b-Log</a>
 			<!--<a class='navi' href='$inst_path/pic2base/bin/admin/admin/md5_add.php' title='kontrolliert auf fehlende Pr&uuml;fsummen'>md5-Check</a>-->
-			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_histogram0.php' title='erstellt fehlende Histogramme'>Histogramme</a>
+			<!--<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_histogram0.php' title='erstellt fehlende Histogramme'>Histogramme</a>-->
 			<!--<a class='navi' href='$inst_path/pic2base/bin/admin/admin/generate_exifdata0.php' title='&uuml;bertr&auml;gt fehlende Meta-Daten aus den Bildern in die DB'>Meta-Daten</a>-->
-			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/db_wartung1.php' title='Datenbank-Wartung mit Dublettenpr&uuml;fung'>DB-Wartung</a>
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/db_wartung1.php' title='Datenbank-Wartung mit Doublettenpr&uuml;fung'>DB-Wartung</a>
+			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/db_export1.php' title='Datenbank-Export als CSV-Datei'>DB-Export</a>
 			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/del_pics1.php' title='Vorgemerkte Datens&auml;tze l&ouml;schen'>Bilder l&ouml;schen</a>
 			<a class='navi_blind'></a>
 			<a class='navi' href='$inst_path/pic2base/bin/admin/admin/protect_metadata0.php' title='Festlegung editierbarer Meta-Daten'>Meta-Protect</a>
