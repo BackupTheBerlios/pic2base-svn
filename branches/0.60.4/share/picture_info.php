0dbf93b09e1be5820bd533c6f9afaa6a7dba4a96
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-10-07 14:03:08 +0000

    alten Code entfernt

--- branches/0.60.4/share/picture_info.php
+++ branches/0.60.4/share/picture_info.php
@@ -44,26 +44,6 @@ if ($inforequest == 'DownloadStatusIconPreview')
 	}
 	else
 	{
-		/*IF(($user_id == $Owner AND hasPermission($c_username, 'downloadmypics', $sr)) OR hasPermission($c_username, 'downloadallpics', $sr))
-		{
-			IF(directDownload($c_username, $sr))
-			//IF($direkt_download > '0')
-			{
-				echo"
-				<SPAN style='cursor:pointer;' onClick='window.open(\"$inst_path/pic2base/bin/share/download_picture.php?FileName=$FileName&c_username=$c_username&pic_id=$pic_id\")'>
-				<img src='$inst_path/pic2base/bin/share/images/download.png' hspace='0' vspace='0' title='Bild direkt herunterladen'/></SPAN>";
-			}
-			ELSE
-			{
-				echo "
-				<SPAN style='cursor:pointer;'>
-				<img src='$inst_path/pic2base/bin/share/images/download_disabled.png' hspace='0' vspace='0' title='Bild darf nicht herutergeladen werden.'/></SPAN>";
-			}
-		}
-		ELSE
-		{
-			$icon[$i6] = "<TD align='center' width='43'><BR></TD>";
-		}*/
 		echo "
 		<SPAN style='cursor:pointer;' onClick='downloadButtonPressed(\"$FileName\",\"$c_username\",\"$pic_id\", 1)'><img src='$inst_path/pic2base/bin/share/images/download.png' hspace='0' vspace='0' title='Bild in den FTP-Download-Ordner kopieren' /></SPAN>";
 	}
