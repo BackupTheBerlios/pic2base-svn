138d4086e105847923e07bb2f48421deea353028
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-10-13 17:40:28 +0000

    Zeichenkodierung von iso-8859-15 auf utf-8 geändert

--- branches/0.60.4/html/admin/adminshowpermissions.php
+++ branches/0.60.4/html/admin/adminshowpermissions.php
@@ -1,47 +1,46 @@
 <?php
+include '../../share/global_config.php';
 IF (!$_COOKIE['login'])
 {
-	include '../../share/global_config.php';
-	//var_dump($sr);
   	header('Location: ../../../index.php');
 }
-
-	mysql_connect ($db_server, $user, $PWD);
-	$result = mysql_query("select * from permissions ORDER BY perm_id DESC");
+	include $sr.'/bin/share/db_connect1.php';
+	$result = mysql_query("select * from $table8 ORDER BY perm_id DESC");
 	$num = mysql_num_rows($result);
 	echo "
 	<center>
 	<table class='normal' border='0'>
-	<tr>
-	<td colspan='4' style='font-size:12pt; text-align:center;'>Berechtigungen - informative Auflistung </td>
-	</tr>
-	
-	<tr style='height:3px;'>
-	<td class='normal' align='center' bgcolor='#FF9900' colspan='4'></TD>
-	</TR>
-	
-	<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>
-	
-	<tr>
-	<td align=left colspan='2' width='50%'><b>Recht-Bezeichnung</b></td>
-	<td align=left colspan='2' width='50%'><b>Kurzbezeichnung</b></td>
-	</tr>";
-	for ($i = 0; $i < $num; $i++)
-	{
-	  echo "<tr>
-	  <td align=left colspan='2'>".mysql_result ($result, $i, "description")."</td>
-	  <td align=left colspan='2'>".mysql_result ($result, $i, "perm_id")." - ".mysql_result ($result, $i, "shortdescription")."</td>
-	  </tr>";
-	}
-	echo "
-	<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>
-	
-	<tr style='height:3px;'>
-	<td class='normal' align='center' bgcolor='#FF9900' colspan='4'></TD>
-	</TR>
-	</table></center>";
+		<tr>
+			<td colspan='4' style='font-size:12pt; text-align:center;'>Berechtigungen - informative Auflistung </td>
+		</tr>
+		
+		<tr style='height:3px;'>
+			<td class='normal' align='center' bgcolor='#FF9900' colspan='4'></TD>
+		</TR>
+		
+		<tr>
+			<td colspan='4'>&nbsp;</td>
+		</tr>
+		
+		<tr>
+			<td align=left colspan='2' width='50%'><b>Recht-Bezeichnung</b></td>
+			<td align=left colspan='2' width='50%'><b>Kurzbezeichnung</b></td>
+		</tr>";
+		for ($i = 0; $i < $num; $i++)
+		{
+		  echo "<tr>
+		  <td align=left colspan='2'>".utf8_encode(mysql_result ($result, $i, "description"))."</td>
+		  <td align=left colspan='2'>".mysql_result ($result, $i, "perm_id")." - ".mysql_result ($result, $i, "shortdescription")."</td>
+		  </tr>";
+		}
+		echo "
+		<tr>
+			<td colspan='4'>&nbsp;</td>
+		</tr>
+		
+		<tr style='height:3px;'>
+			<td class='normal' align='center' bgcolor='#FF9900' colspan='4'></TD>
+		</TR>
+	</table>
+	</center>";
 ?>
