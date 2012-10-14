bfbb9dca506bc0fd5dd758e279687f19b2e92bd7
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-10-14 08:25:35 +0000

    Zeichenkodierung von iso-8859-15 auf utf-8 geändert

--- branches/0.60.4/share/kat_treeview.php
+++ branches/0.60.4/share/kat_treeview.php
@@ -82,7 +82,7 @@ if(!isset($bewertung))
 		$num10 = mysql_num_rows($result10);
 		FOR ($i10=0; $i10<$num10; $i10++)
 		{
-			$kategorie = mysql_result($result10, $i10, 'kategorie');
+			$kategorie = utf8_encode(mysql_result($result10, $i10, 'kategorie'));
 			$parent = mysql_result($result10, $i10, 'parent');
 			$level = mysql_result($result10, $i10, 'level');
 			$kat_id = mysql_result($result10, $i10, 'kat_id');
@@ -202,7 +202,7 @@ IF($KAT_ID=='' OR $KAT_ID == '0')
 	$num10 = mysql_num_rows($result10);
 	FOR ($i10=0; $i10<$num10; $i10++)
 	{
-		$kategorie = mysql_result($result10, $i10, 'kategorie');
+		$kategorie = utf8_encode(mysql_result($result10, $i10, 'kategorie'));
 		$parent = mysql_result($result10, $i10, 'parent');
 		$level = mysql_result($result10, $i10, 'level');
 		$kat_id = mysql_result($result10, $i10, 'kat_id');
