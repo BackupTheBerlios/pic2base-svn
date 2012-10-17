73368a10beb0e10b2467921d5731d3cd70ef97bf
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-10-17 17:50:12 +0000

    Zeichenkodierung von iso-8859-15 auf utf-8 geändert

--- branches/0.60.4/share/kat_treeview.php
+++ branches/0.60.4/share/kat_treeview.php
@@ -5,7 +5,6 @@ include '../share/global_config.php';
 //var_dump($sr);
   header('Location: ../../index.php');
 }
-
 INCLUDE 'global_config.php';
 include $sr.'/bin/share/db_connect1.php';
 include $sr.'/bin/share/functions/ajax_functions.php';
@@ -203,6 +202,7 @@ IF($KAT_ID=='' OR $KAT_ID == '0')
 	FOR ($i10=0; $i10<$num10; $i10++)
 	{
 		$kategorie = utf8_encode(mysql_result($result10, $i10, 'kategorie'));
+		//$kategorie = (mysql_result($result10, $i10, 'kategorie'));
 		$parent = mysql_result($result10, $i10, 'parent');
 		$level = mysql_result($result10, $i10, 'level');
 		$kat_id = mysql_result($result10, $i10, 'kat_id');
