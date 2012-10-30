6c8667da8664b92c69555e9be919791e9ade9722
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-10-30 17:20:59 +0000

    individuelle Rechtevergabe unabhängig der zugewiesenen Gruppe ergänzt

--- branches/0.60.4/share/change_userpermission.php
+++ branches/0.60.4/share/change_userpermission.php
@@ -1,5 +1,5 @@
 <?php
-IF (!$_COOKIE['login'])
+IF (!$_COOKIE['uid'])
 {
 	include '../share/global_config.php';
 	//var_dump($sr);
@@ -9,10 +9,7 @@ IF (!$_COOKIE['login'])
 $sr = $_GET['sr'];
 
 include $sr.'/bin/share/db_connect1.php';
-//include $sr.'/bin/share/functions/ajax_functions.php';
-//include $sr.'/bin/share/functions/main_functions.php';
 
-//var_dump($_GET);
 if ( array_key_exists('user_id',$_GET) )
 {
 	$user_id = $_GET['user_id'];
@@ -38,9 +35,19 @@ ELSEIF($checked == 'checked')
 	$checked = '';
 	$text = 'keine Berechtigung';
 }
-
-$result1 = mysql_query( "UPDATE $table7 SET enabled = '$new_status' WHERE permission_id = '$perm_id' AND user_id = '$user_id'");
-echo mysql_error();
+//es wird kontrolliert, ob dem User durch die Gruppenzugehoerigkeit bereits das zu aendernde Recht zugewiesen wurde. Wenn ja, wird dies wie gewuenscht geaendert,
+//wenn nein, wird dieses Recht zunaechst zugefuegt:
+$result1 = mysql_query("SELECT * FROM $table7 WHERE permission_id = '$perm_id' AND user_id = '$user_id'");
+if(mysql_num_rows($result1) == 1)
+{
+	$result2 = mysql_query( "UPDATE $table7 SET enabled = '$new_status' WHERE permission_id = '$perm_id' AND user_id = '$user_id'");
+	echo mysql_error();
+}
+else
+{
+	$result2 = mysql_query( "INSERT INTO $table7 (enabled, permission_id, user_id) VALUES ('$new_status', '$perm_id', '$user_id')");
+	echo mysql_error();
+}
 mysql_close($conn);
 
 echo "<INPUT TYPE=CHECKBOX $checked value='$new_status' title= '$text' onClick='changeUserpermission(\"$user_id\",\"$perm_id\",\"$checked\",\"$sr\")'>";
