ce1c2845feb1e2e5e32cffaa5b486e4a763eab84
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-10-31 10:58:37 +0000

    Benutzeridentifizierung von $c_username auf $uid umgestellt

--- branches/0.60.4/share/functions/permissions.php
+++ branches/0.60.4/share/functions/permissions.php
@@ -1,16 +1,16 @@
 <?php 
 
-function hasPermission($username, $permissionString, $sr)
+function hasPermission($uid, $permissionString, $sr)
 {
 	error_reporting($_SERVER["SERVER_NAME"] == "localhost" ? E_ALL : 0);
 	include $sr.'/bin/share/global_config.php';
 	include $sr.'/bin/share/db_connect1.php';
-	$result1 = mysql_query("SELECT $table1.id, $table1.username, $table1.aktiv, $table7.user_id, $table7.permission_id, 
+	$result1 = mysql_query("SELECT $table1.id, $table1.aktiv, $table7.user_id, $table7.permission_id, 
 	$table7.enabled, $table8.perm_id, $table8.shortdescription
 	FROM $table1 inner join $table7 inner join $table8
 	ON $table1.id = $table7.user_id 
 	AND $table7.permission_id = $table8.perm_id
-	AND $table1.username = '$username'
+	AND $table1.id = '$uid'
 	AND $table1.aktiv = '1'
 	AND $table8.shortdescription = '$permissionString' 
 	AND $table7.enabled = '1'");
