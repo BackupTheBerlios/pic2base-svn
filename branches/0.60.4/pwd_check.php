ddf46253485264a4296f78387feb631b3c3c4824
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-11-03 09:29:19 +0000

    bei Fehleingabe wir der focus auf den Zurück-Button gesetzt

--- branches/0.60.4/pwd_check.php
+++ branches/0.60.4/pwd_check.php
@@ -12,7 +12,9 @@
 </head>
 
 <body>
-<!--
+
+<?php
+
 /*
  * Project: pic2base
  * File: pwd_check.php
@@ -27,74 +29,83 @@
  * All files of this project are licensed under the terms of the Open Software License
  * http://www.opensource.org/licenses/osl-2.1.php
  */
- -->
-<DIV Class="klein">
- 
-<?php 
-$ACTION = $_SERVER['PHP_SELF'];
-$link = "http://{$_SERVER['SERVER_NAME']}$ACTION";
+
+echo "<DIV Class='klein'>"; 
+//	$ACTION = $_SERVER['PHP_SELF'];
+//	$link = "http://{$_SERVER['SERVER_NAME']}$ACTION";
+	
+	echo "
+	<div class='page'>
+	
+		<p id='kopf'>pic2base :: Zugangskontrolle</p>
+		
+		<div class='navi' style='clear:right;'>
+			<div class='menucontainer'>
+			</div>
+		</div>
+		
+		<div class='content'>
+			<p style='margin:170px 0px; text-align:center'>";
+			
+				include 'share/global_config.php';
+				include $sr.'/bin/share/db_connect1.php';
+				
+				// Fehlermeldung, falls ungueltige Daten eingegeben wurden:
+				$text = "<CENTER>
+				<p class='zwoelfred' align='center'><b>Sie haben fehlerhafte Zugangsdaten eingegeben!<BR>
+				oder Ihr Account wurde gesperrt.<BR>
+				Der Vorgang wird abgebrochen.</b>
+				</p><BR>
+				<INPUT TYPE='button' id='back' Value='Zur&uuml;ck' OnClick='location.href=\"../index.php\"'>
+				</CENTER>
+				<script type='text/javascript'>
+				document.getElementById('back').focus();
+				</script>";
+			
+				if( isset($_POST['username']) )
+				{
+					if( !empty($_POST['username']) )
+					{
+			                	$username = utf8_decode($_POST['username']);
+			                    $passwd = utf8_decode($_POST['passwd']);
+			                    $result1 = mysql_query("SELECT * FROM $table1 WHERE username = '$username' AND pwd = ENCRYPT('$passwd','$key') AND aktiv = '1'");
+								//echo mysql_error();
+								@$num1 = mysql_num_rows($result1);
+								if ($num1 > '0')
+								{
+									$uid = mysql_result($result1, isset($i1), 'id');
+									setcookie('uid',$uid,0,'/');
+									echo "<CENTER>
+									<p class='mittel'>Zugangspr&uuml;fung l&auml;uft...</p>
+									<img src='share/images/loading.gif' width='20' height='20' />
+									</CENTER>
+									<meta http-equiv='refresh' content = '0; URL=html/start.php?check=1'>";
+								}
+								else
+								{
+									$num1 = 0;
+							        echo $text;
+								}
+			        }
+			        else
+			        {
+			        	$num1 = 0;
+			        	echo $text;
+			        }
+				}
+				
+				@mysql_close($conn);
+			
+			echo "
+			</p>
+		</div>
+		<br style='clear:both;'' />
+	
+		<p id='fuss'><A style='margin-right:745px; color:#eeeeee;' HREF='http://www.pic2base.de' target='blank'>www.pic2base.de</A>".$cr."</p>
+	
+	</div>
+</DIV>";
 ?>
 
-<div class="page">
-
-	<p id="kopf">pic2base :: Zugangskontrolle</p>
-	
-	<div class="navi" style="clear:right;">
-		<div class="menucontainer">
-		</div>
-	</div>
-	
-	<div class="content">
-	<p style="margin:170px 0px; text-align:center">
-	
-	<?php 
-	include 'share/global_config.php';
-	include $sr.'/bin/share/db_connect1.php';
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//var_dump($_POST);
-	if( isset($_POST['username']) )
-	{
-		if( !empty($_POST['username']) )
-	        {
-                	$username = utf8_decode($_POST['username']);
-                    $passwd = utf8_decode($_POST['passwd']);
-            }
-	}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
-	$result1 = mysql_query("SELECT * FROM $table1 WHERE username = '$username' AND pwd = ENCRYPT('$passwd','$key') AND aktiv = '1'");
-	//echo mysql_error();
-	@$num1 = mysql_num_rows($result1);
-	IF ($num1 > '0')
-	{
-		$uid = mysql_result($result1, isset($i1), 'id');
-		setcookie('uid',$uid,0,'/');
-		echo "<CENTER>
-		<p class='mittel'>Zugangspr&uuml;fung l&auml;uft...</p>
-		<img src='share/images/loading.gif' width='20' height='20' />
-		</CENTER>
-		<meta http-equiv='refresh' content = '0; URL=html/start.php?check=1'>";
-	}
-	else
-	{
-		echo "
-		<CENTER>
-		<p class='zwoelfred' align='center'><b>Sie haben fehlerhafte Zugangsdaten eingegeben!<BR>
-		oder Ihr Account wurde gesperrt.<BR>
-		Der Vorgang wird abgebrochen.</b></p><BR>
-		<INPUT TYPE='button' Value='Zur&uuml;ck' OnClick='location.href=\"../index.php\"'>
-		</CENTER>";
-	}
-	@mysql_close($conn);
-	?>
-	
-	</p>
-	</div>
-	<br style="clear:both;" />
-
-	<p id="fuss"><A style='margin-right:745px; color:#eeeeee;' HREF='http://www.pic2base.de' target='blank'>www.pic2base.de</A><?php echo $cr; ?></p>
-
-</div>
-</DIV>
 </body>
 </html>
