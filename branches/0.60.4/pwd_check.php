37010b3037863f8562ea8f31ec3466889ed9f786
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2012-11-07 16:55:45 +0000

    korrektes Cookie-setzen durch Umstellung der Code-Reihenfolge

--- branches/0.60.4/pwd_check.php
+++ branches/0.60.4/pwd_check.php
@@ -1,4 +1,55 @@
-<?php //setcookie('login',$_REQUEST['username'],0,'/');?>
+<?php
+if (!isset($_POST['username']))
+{
+	header('Location: ../index.php');
+}
+else
+{
+	include 'share/global_config.php';
+	include $sr.'/bin/share/db_connect1.php';
+	$success = "<CENTER>
+	<p class='mittel'>Zugangspr&uuml;fung l&auml;uft...</p>
+	<img src='share/images/loading.gif' width='20' height='20' />
+	</CENTER>
+	<meta http-equiv='refresh' content = '0; URL=html/start.php?check=1'>";
+	$error = "<CENTER>
+	<p class='zwoelfred' align='center'><b>Sie haben fehlerhafte Zugangsdaten eingegeben!<BR>
+	oder Ihr Account wurde gesperrt.<BR>
+	Der Vorgang wird abgebrochen.</b>
+	</p>
+	<BR>
+	<INPUT TYPE='button' id='back' Value='Zur&uuml;ck' OnClick='location.href=\"html/login1.php\"'>
+	</CENTER>
+	<script type='text/javascript'>
+	document.getElementById('back').focus();
+	</script>";
+	if( !empty($_POST['username']) )
+	{
+		$username = utf8_decode($_POST['username']);
+        $passwd = utf8_decode($_POST['passwd']);
+        $result1 = mysql_query("SELECT * FROM $table1 WHERE username = '$username' AND pwd = ENCRYPT('$passwd','$key') AND aktiv = '1'");
+		@$num1 = mysql_num_rows($result1);
+		
+		if ($num1 > '0')
+		{
+			$uid = mysql_result($result1, isset($i1), 'id');
+			setcookie('uid',$uid,0,'/');
+			$meldung = $success;
+			@mysql_close($conn);
+		}
+		else
+		{
+			$num1 = 0;
+		    $meldung = $error;
+		}
+	}
+    else
+    {
+    	$num1 = 0;
+        $meldung = $error;
+    }
+}
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 
@@ -30,78 +81,22 @@
  * http://www.opensource.org/licenses/osl-2.1.php
  */
 
-echo "<DIV Class='klein'>"; 
-//	$ACTION = $_SERVER['PHP_SELF'];
-//	$link = "http://{$_SERVER['SERVER_NAME']}$ACTION";
+echo "<DIV Class='klein'>
+		<div class='page'>
 	
-	echo "
-	<div class='page'>
-	
-		<p id='kopf'>pic2base :: Zugangskontrolle</p>
-		
-		<div class='navi' style='clear:right;'>
-			<div class='menucontainer'>
+			<p id='kopf'>pic2base :: Zugangskontrolle</p>
+			
+			<div class='navi' style='clear:right;'>
+				<div class='menucontainer'>
+				</div>
 			</div>
-		</div>
+			
+			<div class='content'>
+				<p style='margin:80px 0px; text-align:center'>".$meldung."</p>
+			</div>
+			<br style='clear:both;'' />
 		
-		<div class='content'>
-			<p style='margin:80px 0px; text-align:center'>";
-			
-				include 'share/global_config.php';
-				include $sr.'/bin/share/db_connect1.php';
-				
-				// Fehlermeldung, falls ungueltige Daten eingegeben wurden:
-				$text = "<CENTER>
-				<p class='zwoelfred' align='center'><b>Sie haben fehlerhafte Zugangsdaten eingegeben!<BR>
-				oder Ihr Account wurde gesperrt.<BR>
-				Der Vorgang wird abgebrochen.</b>
-				</p><BR>
-				<INPUT TYPE='button' id='back' Value='Zur&uuml;ck' OnClick='location.href=\"html/login1.php\"'>
-				</CENTER>
-				<script type='text/javascript'>
-				document.getElementById('back').focus();
-				</script>";
-			
-				if( isset($_POST['username']) )
-				{
-					if( !empty($_POST['username']) )
-					{
-			                	$username = utf8_decode($_POST['username']);
-			                    $passwd = utf8_decode($_POST['passwd']);
-			                    $result1 = mysql_query("SELECT * FROM $table1 WHERE username = '$username' AND pwd = ENCRYPT('$passwd','$key') AND aktiv = '1'");
-								//echo mysql_error();
-								@$num1 = mysql_num_rows($result1);
-								if ($num1 > '0')
-								{
-									$uid = mysql_result($result1, isset($i1), 'id');
-									setcookie('uid',$uid,0,'/');
-									echo "<CENTER>
-									<p class='mittel'>Zugangspr&uuml;fung l&auml;uft...</p>
-									<img src='share/images/loading.gif' width='20' height='20' />
-									</CENTER>
-									<meta http-equiv='refresh' content = '0; URL=html/start.php?check=1'>";
-								}
-								else
-								{
-									$num1 = 0;
-							        echo $text;
-								}
-			        }
-			        else
-			        {
-			        	$num1 = 0;
-			        	echo $text;
-			        }
-				}
-				
-				@mysql_close($conn);
-			
-			echo "
-			</p>
-		</div>
-		<br style='clear:both;'' />
-	
-		<p id='fuss'><A style='margin-right:745px; color:#eeeeee;' HREF='http://www.pic2base.de' target='blank'>www.pic2base.de</A>".$cr."</p>
+			<p id='fuss'><A style='margin-right:745px; color:#eeeeee;' HREF='http://www.pic2base.de' target='blank'>www.pic2base.de</A>".$cr."</p>
 	
 	</div>
 </DIV>";
