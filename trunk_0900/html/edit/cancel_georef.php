4e5b19771b71a85dac09c644b76ba13812cf9b49
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2013-09-29 10:05:02 +0000

    Syntax-Fehler in SQL-Statement behoben

--- trunk_0900/html/edit/cancel_georef.php
+++ trunk_0900/html/edit/cancel_georef.php
@@ -24,15 +24,15 @@ if ( array_key_exists('userid',$_GET) )
 	$userid = $_GET['userid'];
 }
 echo "Georeferenzierung wird abgebrochen...<BR>";
-//echo "&Uuml;bergebener Username: ".$uname."<BR>";
+//echo "&Uuml;bergebene User-ID: ".$userid."<BR>";
 include '../../share/global_config.php';
 include $sr.'/bin/share/db_connect1.php';
 
 $result1 = mysql_query("SELECT $table2.Owner, $table2.pic_id, $table2.City
 FROM $table2
 WHERE $table2.Owner = $userid
-AND ($table2.City = 'Ortsbezeichnung' OR $table2.City = '' OR $table2.City = 'skipped'");
-//echo mysql_error();
+AND ($table2.City = 'Ortsbezeichnung' OR $table2.City = '' OR $table2.City = 'skipped')");
+echo mysql_error();
 $num1 = mysql_num_rows($result1);
 //echo $num1." Treffer.<BR>";
 FOR ($i1='0'; $i1<$num1; $i1++)
@@ -42,8 +42,9 @@ FOR ($i1='0'; $i1<$num1; $i1++)
 	echo mysql_error();
 }
 //dann zurueck zur Edit-Start-Seite:
-echo "<meta http-equiv = 'refresh', content='0; url=edit_start.php' >";
+echo "<meta http-equiv = 'refresh', content='1; url=edit_start.php' >";
 //08.06.2011 (kh.)
+//29.09.2013 (kh.)
 ?>
 
 </body>
