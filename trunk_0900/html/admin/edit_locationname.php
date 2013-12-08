578b4956f7753dcee962b74425d5dfa36fda4e68
klaus_h <klaus_h@9cac30e5-4674-0410-b0cd-eb4b23da8320>
2013-12-08 16:46:37 +0000

    Formatierungsfehler bei sehr vielen Ortsnamen (langer Liste) korrigiert

--- trunk_0900/html/admin/edit_locationname.php
+++ trunk_0900/html/admin/edit_locationname.php
@@ -27,52 +27,52 @@ $num = mysql_num_rows($result);
 $city = mysql_result ($result, isset($i), "City");
 echo "
   <center>
-  <FORM name='location' method='post' action='adminframe.php?item=admineditlocationnameaction&pic_id=$id'>
-  <table class='normal' border='0'>
-	<tr>
-	<td colspan='4' style='font-size:12pt; text-align:center;'>Ortsnamen bearbeiten</td>
-	</tr>
-	
-	<tr style='height:3px;'>
-	<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
-	</TR>
-	
-	<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>
-	
-  	<TR>
-	<TD align='CENTER' colspan='4' style='padding-left:20px;'>
-	<INPUT TYPE='text' name='locationname_new' VALUE=\"$city\" style='width:400px;'></INPUT>
-	<INPUT TYPE='hidden' name='locationname' VALUE=\"$city\" style='width:400px;'></INPUT>
-	</TD>
-	</TR>
-	
-	<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>
-	
-	<TR>
-	<TD align='CENTER' colspan='4' style='padding-left:20px;'>
-	<INPUT TYPE=submit VALUE='Neue Ortsbezeichnung speichern'>
-	<!--<INPUT TYPE='button' VALUE='Abbrechen' onClick='location.href=\"adminframe.php?item=editlocationname\"'>-->
-	
-	<INPUT TYPE='button' VALUE='Abbrechen' onClick='javascript:history.back()'>	
-	</TD>
-	</TR>
-	
-	<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>
-	
-	<tr style='height:3px;'>
-	<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
-	</TR>
-  
-  	<TR>
-	<TD align='center' colspan = '4'></TD>
-	</TR>
-	</table>
+  	<FORM name='location' method='post' action='adminframe.php?item=admineditlocationnameaction&pic_id=$id'>
+	  	<table class='normal' border='0'>
+			<tr>
+				<td colspan='4' style='font-size:12pt; text-align:center;'>Ortsnamen bearbeiten</td>
+			</tr>
+			
+			<tr style='height:3px;'>
+				<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
+			</TR>
+			
+			<tr>
+				<td colspan='4'>&nbsp;</td>
+			</tr>
+			
+		  	<TR>
+				<TD align='CENTER' colspan='4' style='padding-left:20px;'>
+				<INPUT TYPE='text' name='locationname_new' VALUE=\"$city\" style='width:400px;'></INPUT>
+				<INPUT TYPE='hidden' name='locationname' VALUE=\"$city\" style='width:400px;'></INPUT>
+				</TD>
+			</TR>
+			
+			<tr>
+				<td colspan='4'>&nbsp;</td>
+			</tr>
+			
+			<TR>
+				<TD align='CENTER' colspan='4' style='padding-left:20px;'>
+				<INPUT TYPE=submit VALUE='Neue Ortsbezeichnung speichern'>
+				<!--<INPUT TYPE='button' VALUE='Abbrechen' onClick='location.href=\"adminframe.php?item=editlocationname\"'>-->
+			
+				<INPUT TYPE='button' VALUE='Abbrechen' onClick='javascript:history.back()'>	
+				</TD>
+			</TR>
+			
+			<tr>
+				<td colspan='4'>&nbsp;</td>
+			</tr>
+			
+			<tr style='height:3px;'>
+				<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
+			</TR>
+		  
+		  	<TR>
+				<TD align='center' colspan = '4'></TD>
+			</TR>
+		</table>
 	</FORM>
 </center>";
 ?>
--- trunk_0900/html/admin/show_locationname.php
+++ trunk_0900/html/admin/show_locationname.php
@@ -16,39 +16,53 @@ else
 //#########################################################################
 $result = mysql_query("select * from $table2 WHERE City <> 'Ortsbezeichnung' GROUP BY City ORDER BY City");
 $num = mysql_num_rows($result);
-echo "<center><table class='normal' border='0'>
-	<tr>
-	<td colspan='4' style='font-size:12pt; text-align:center;'>&Uuml;bersicht &uuml;ber alle vorhandenen Ortsnamen</td>
-	</tr>
+echo "
+<center>
+<div id='names' style='width:530px;height:550px;overflow-y:scroll;'>
+	<table class='normal' border='0'>
+		<tr style='height:3px;'>
+			<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
+		</TR>
+		
+		<tr>
+			<td colspan='4' style='font-size:13pt; text-align:center;'>&Uuml;bersicht &uuml;ber alle vorhandenen Ortsnamen</td>
+		</tr>
+		
+		<TR>
+			<TD align='center' colspan = '4'>Klicken Sie auf einen Ortsnamen um diesen zu bearbeiten</TD>
+		</TR>
+		
+		<tr style='height:3px;'>
+			<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
+		</TR>
+		
+		<tr>
+			<td colspan='4'>&nbsp;</td>
+		</tr>";
 	
-	<tr style='height:3px;'>
-	<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
-	</TR>
-	
-	<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>";
-	
-FOR ($i = 0; $i < $num; $i++)
-{
-	$pic_id = mysql_result ($result, $i, "pic_id");
-	$city = mysql_result ($result, $i, "City");
-	
-	echo "<TR>
-	<TD align='left' colspan='4' style='padding-left:20px;'><a href=adminframe.php?item=admineditlocation&id=".$pic_id.">".$city."</a></TD>
-	</TR>";
-}
+		FOR ($i = 0; $i < $num; $i++)
+		{
+			$pic_id = mysql_result ($result, $i, "pic_id");
+			$city = mysql_result ($result, $i, "City");
+			
+			echo "<TR>
+			<TD align='left' colspan='4' style='padding-left:20px;'><a href=adminframe.php?item=admineditlocation&id=".$pic_id.">".$city."</a></TD>
+			</TR>";
+		}
   
-echo "<tr>
-	<td colspan='4'>&nbsp;</td>
-	</tr>
-	
-	<tr style='height:3px;'>
-	<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
-	</TR>
-  
-  	<TR>
-	<TD align='center' colspan = '4'>Klicken Sie auf einen Ortsnamen um diesen zu bearbeiten</TD>
-	</TR>
-	</table></center>";
+echo "	<tr>
+			<td colspan='4'>&nbsp;</td>
+		</tr>
+		
+		<tr style='height:3px;'>
+			<td class='normal' align='center' bgcolor='darkred' colspan='4'></TD>
+		</TR>
+		
+		<tr>
+			<td colspan='4'>&nbsp;</td>
+		</tr>
+	  
+	</table>
+	</div>
+</center>";
 ?>
